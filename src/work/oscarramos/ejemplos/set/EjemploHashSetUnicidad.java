8656ee5d50d938c3b4391655e32a5c3e54dd20c2
Oscar Andres Ramos Lopez <oscar1940_1@hotmail.com>
2022-12-20 09:11:07 -0500

    Tertminacion del ejercicio de unidad
    aqui se aprendio que para los tipos List y LinkendList se
    puede usar una manera adicional para recorrer este tipo de
    estructura que seria los for normales ya que estos no poseen
    algunos metodos requeridos para usar este tipo de esctructura

--- src/work/oscarramos/ejemplos/set/EjemploHashSetUnicidad.java
+++ src/work/oscarramos/ejemplos/set/EjemploHashSetUnicidad.java
@@ -2,13 +2,22 @@ package work.oscarramos.ejemplos.set;
 
 import work.oscarramos.ejemplos.modelo.Alumno;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class EjemploHashSetUnicidad {
     public static void main(String[] args) {
+        /*
+            Esto aplica tanto para HashSet como para TreeSet
+            La diferencia con las lista radica en estas estan
+            en orden de ingresos de los datos mientras que el
+            TreeSet ordena por defecto al igual HashSet
+         */
+        //Set<Alumno> sa = new HashSet<>();
+        //Set<Alumno> sa = new TreeSet<>();
 
-        Set<Alumno> sa = new HashSet<>();
+        //List<Alumno> sa = new ArrayList<>();
+        List<Alumno> sa = new LinkedList<>();
+
 
         sa.add(new Alumno("Oscar", 9));
         sa.add(new Alumno("Paola", 8));
@@ -21,8 +30,42 @@ public class EjemploHashSetUnicidad {
         sa.add(new Alumno("Roberto", 4));
         sa.add(new Alumno("Sebastian", 10));
 
+        System.out.println("======================================");
+        System.out.println("impresion de objeto");
+        System.out.println("======================================");
         System.out.println(sa);
 
+        System.out.println("======================================");
+        System.out.println("Iterando usando un foreach");
+        System.out.println("======================================");
 
+        for (Alumno a: sa){
+            System.out.println(a);
+        }
+
+        System.out.println("======================================");
+        System.out.println("Iterando usando un while y el iterator");
+        System.out.println("======================================");
+
+        Iterator<Alumno> it = sa.iterator();
+
+        while(it.hasNext()){
+            Alumno a = it.next();
+            System.out.println(a);
+        }
+        System.out.println("======================================");
+        System.out.println("Iterando usando Stream forEach");
+        System.out.println("======================================");
+
+        sa.forEach(System.out::println);
+
+        System.out.println("===========================================");
+        System.out.println("Usando un for normal solo sirve para listas");
+        System.out.println("===========================================");
+
+        for (int i=0; i < sa.size(); i++) {
+            Alumno a = sa.get(i);
+            System.out.println(a);
+        }
     }
 }
